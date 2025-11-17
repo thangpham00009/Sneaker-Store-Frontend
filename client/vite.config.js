40bfe83e517de41f029ce0f5298dc2956e59a4c1
Thang Pham <thangpham00009@gmail.com>
2025-11-17 17:31:38 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@
 /* eslint-env node */
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -12,6 +13,11 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'), // <-- thêm dòng này
+      },
+    },
     server: {
       proxy: {
         "/api": {
